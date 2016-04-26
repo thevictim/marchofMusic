89279c454e7e3fb0bfcf7b159ece442d76a15a46
Guanyu Chen <decrecyc@gmail.com>
2016-04-26 00:11:42 -0500

    done adding

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
-// app.use(express.static(__dirname + '/public'));
-
-var allsong;
 
 mongoose.connect(process.env.MONGODB_URI ,function(err){
   if(err) throw err;
@@ -21,143 +18,50 @@ mongoose.connect(process.env.MONGODB_URI ,function(err){
   });
 });
 
-
-
 var db = mongoose.connection;
 
 var albumSchema = mongoose.Schema({
   artist: String,
   name: String, 
-  year: Number,
+  year: String,
   imageurl: String
 });
 var Album = mongoose.model('albums', albumSchema);
 
-db.once('open', function callback (){
-
-  // Create seed data
-  var pf = new Album({
-    artist: 'Pink Floyd',
-    name: 'Dark Side of the Moon', 
-    year: 1971,
-    imageurl: 'asdffasd'
-  });
-
-
-  pf.save();
-
-});
-
-
 app.post("/sendList", function(req, res) {
-  console.log('------------------------node post------------------------');
-  var albumList = req.body.list;
-  console.log('parse');
-  console.log(albumList);
-  if (!req.body.list) {
+  console.log('------------------------post to node------------------------');
+  var album = req.body;
+  if (!req.body) {
     handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
   }
 
-  // var searchAlbums = function (query) {
-  //   $.ajax({
-  //     url: 'https://api.spotify.com/v1/search',
-  //     data: {
-  //       q: query,
-  //       type: 'album'
-  //     },
-  //     success: function (response) {
-  //       for (album in response.albums.items){
+  var newAlbum = new Album({
+    artist: album.artist,
+    name: album.name, 
+    year: album.year,
+    imageurl: album.imageurl
+  });
 
-  //         var newAlbum = new Album({
-  //           artist: album.artist.name,
-  //           name: album.name, 
-  //           year: album.release_date,
-  //           imageurl: album.images[0].url
-  //         });
+  newAlbum.save(function(err, doc){
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc);
+    }
+  });
+  console.log('------------------------save completed------------------------');
+});
 
-  //         db.collection(albums).insertOne(newAlbum, function(err, doc) {
-  //           if (err) {
-  //             handleError(res, err.message, "Failed to create new contact.");
-  //           } else {
-  //             res.status(201).json(doc.ops[0]);
-  //           }
-  //         });
+app.get("/getAlbums", function(req, res){
 
-  //       }      
-  //     }
-  //   });
-  // };
-  for (album in albumList){
-    var newAlbum = new Album({
-      artist: album.artist,
-      name: album.name, 
-      year: album.year,
-      imageurl: album.imageurl
-    });
+  Album.find().lean().exec(function(err, albums){
+    return res.end(JSON.stringify(albums));
+  })
 
-    Album.collection.insert(newAlbum, function(err, doc) {
-      if (err) {
-        handleError(res, err.message, "Failed to create new contact.");
-      } else {
-        res.status(201).json(doc.ops[0]);
-      }
-    });
-
-  }
-
-
-  // searchAlbums(newArtist);
-  // console.log(newArtist);
 });
 
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
   res.status(code || 500).json({"error": message});
 }
-
-
-
-
-
-// app.post("/sendArtist", function(req, res){
-//     var newArtist = req.body;
-// });
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
-// app.get('/db', function (request, response) {
-//   // pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//   //   client.query('SELECT * FROM test_table', function(err, result) {
-//   //     done();
-//   //     if (err)
-//   //      { console.error(err); response.send("Error " + err); }
-//   //     else
-//   //      { response.render('pages/db', {results: result.rows} ); }
-//   //   });
-//   // });
-//   var result = ['haha'];
-//   allsong.exec(function(err, docs){
-//         if (err) throw err;
-//         docs.forEach(function(doc){
-//           // console.log( 'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] + 
-//           //   ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.');
-//         var temp = doc['song'];
-
-//         if (result.indexOf(temp)== -1) {
-//           result.push(temp);
-
-//         }
-//         });
-//   // console.log("inner:" + result);
-//     response.send(result);
-//       });
-
-// });
-
-// app.get('/', function(request, response) {
-//   // response.send('ga');
-//   // response.sendFile(path.join(__dirname + '/index.html'));
-// });
-
 
--- public/js/app.js
+++ public/js/app.js
@@ -5,66 +5,63 @@ var myapp = angular.module('trashApp', [
   'angular-timeline', 'ngRoute','ngSanitize',
   'ui.router',
   'angular-scroll-animate'])
-.config(function($stateProvider) {
-  $stateProvider.state('user', {
-    url:         '',
+.config(function($routeProvider) {
+  $routeProvider.when('/', {
     controller: 'ExampleCtrl',
-    templateUrl: 'example.html'
+    templateUrl: 'example.html',
+    resolve: {
+        'albums': function(Albums){
+          console.log('***********************');
+          // addEvent();
+          return Albums.getAlbums();
+        }
+    }
   });
 }).service("Albums", ["$http",function($http){
-  this.sendSearch = function(albumList) {
+  this.sendSearch = function(album) {
     // console.log(albumList);
-    return $http.post("/sendList",albumList).
+    return $http.post("/sendList",album).
     then(function(response) {
       return response;
     }, function(response) {
       // console.log(response);
-      alert("Error finding contacts.");
+      alert("Cannot find artist.");
     });
   }
-
+this.getAlbums = function(){
+  return $http.get("/getAlbums").
+        then(function(response){
+          return response;
+        }, function(response){
+          alert("Error getting albums");
+        });
+}
 
 }])
-.controller('ExampleCtrl', function($scope, Albums) {
-  var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. " +
-  "Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor." +
-  "Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, " +
-  "ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor." +
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+.controller('ExampleCtrl', ['$scope', 'albums', 'Albums', function($scope, albums, Albums){
+  var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. ";
 
   $scope.side = '';
-
-  $scope.events = [{
-      badgeClass: 'info',
-      badgeIconClass: 'glyphicon-check',
-      title: 'First heading',
-      when: '11 hours ago via Twitter',
-      content: 'Some awesome content.'
-      }, {
-      badgeClass: 'warning',
-      badgeIconClass: 'glyphicon-credit-card',
-      title: 'Second heading',
-      when: '12 hours ago via Twitter',
-      content: 'More awesome content.'
-      }, {
-      badgeClass: 'default',
-      badgeIconClass: 'glyphicon-credit-card',
-      title: 'Third heading',
-      titleContentHtml: '<img class="img-responsive" src="http://www.freeimages.com/assets/183333/1833326510/wood-weel-1444183-m.jpg">',
-      contentHtml: lorem,
-      footerContentHtml: '<a href="">Continue Reading</a>'
-    }];
-
-  $scope.addEvent = function(artist, albumName, year, imageurl) {
+  $scope.events = [];
+  $scope.addEvent = function(artist, albumName, year,month, imageurl) {
     $scope.events.push({
       badgeClass: 'info',
       badgeIconClass: 'glyphicon-check',
       title: albumName,
-      when: year,
+      when: year+' '+month,
+      year: year,
+      month: month,
       contentHtml:'<img class="img-responsive" src="'+imageurl+'">'
     });
 
   };
+    albums.data.forEach(function(album){
+      var when = album.year.toString();
+      var year = when.substring(0,4);
+      var month = when.length>4 ? when.substring(5,7):'';
+
+    $scope.addEvent(album.artist, album.name, year,month, album.imageurl);
+  });
         // optional: not mandatory (uses angular-scroll-animate)
         $scope.animateElementIn = function($el) {
           $el.removeClass('timeline-hidden');
@@ -89,11 +86,11 @@ var myapp = angular.module('trashApp', [
             $scope.side = ''; // or 'alternate'
           }
 
-          this.sendArtist = function(){
-            var query = this.artist;
+        $scope.sendArtist = function(){
+            var token = 'BQA3bItLVFgV6_pBf1MsXbpngPoSMkz5JIKuJeT7uLycuQ2-AuGweEXYIKhVRNl3BF3ipFZtoBO_Z8kUs3NO2vbPZjoQTPxlA773LsyXNpy0Ec-4BSzR6IAm5gZVHmUB6HfjBgg1nLdQ';
+            var query = $scope.artist;
             console.log(query);
             var albumList = {list: []};
-
             $.ajax({
               url: 'https://api.spotify.com/v1/search',
               data: {
@@ -104,45 +101,43 @@ var myapp = angular.module('trashApp', [
               },
               success: function (response){        
                 var artist_id = response.artists.items[0].id;
-                console.log(artist_id);
                 $.ajax({
-                  url: 'https://api.spotify.com/v1/artists/'+ artist_id+'/ablums',
+                  url: 'https://api.spotify.com/v1/artists/'+ artist_id+'/albums',
                   headers: {
-                            "Authorization": 'Bearer BQByKBNgoSTPj2C88h9fzwg1FYu08ruyNxFPhXlqboux9uQJaBGB_g7cTXZmbSkb1cbuLXGYskTj-DlFQJBIxM6wGUHt3mvlV-3SscSYKRlCj6rnl_6WPclybgshqX9KjM4pLgPcuuwe'
+                            "Authorization": 'Bearer '+ token
                           },
                   data: {
                         album_type: 'album',
-                        limit: 20
+                        limit: 10,
+                        offset: 10 
+
                   },
                   success: function(response){
                     response.items.forEach(function(album){
-// -------------------
                         var album_id = album.id;
                         $.ajax({
                           url: 'https://api.spotify.com/v1/albums/'+album_id,
                           headers: {
-                            "Authorization": 'Bearer BQByKBNgoSTPj2C88h9fzwg1FYu08ruyNxFPhXlqboux9uQJaBGB_g7cTXZmbSkb1cbuLXGYskTj-DlFQJBIxM6wGUHt3mvlV-3SscSYKRlCj6rnl_6WPclybgshqX9KjM4pLgPcuuwe'
+                            "Authorization": 'Bearer '+ token
                           },
                           data: {
                               market: 'US'
                           },
                           success: function(response){
-                              var newAlbum = {
-                                artist: response.artists[0].name,
-                                name: response.name, 
-                                year: response.release_date,
-                                imageurl: response.images[0].url
-                              };
-                              console.log(response.release_date);
-                              albumList.list.push(newAlbum);
+                            var newAlbum = {
+                              artist: response.artists[0].name,
+                              name: response.name, 
+                              year: response.release_date,
+                              imageurl: response.images[0].url
+                            };
+                            var when = response.release_date.toString();
+                            var year = when.substring(0,4);
+                            var month = when.length>4 ? when.substring(5,7):'';
+                            $scope.addEvent(response.artists[0].name, response.name, year, month, response.images[0].url);
+                            Albums.sendSearch(newAlbum);
                           }
                         });
-// -------------------
-
                       }); 
-                      
-                      Albums.sendSearch(albumList);
-
                   }
                 });
               }
@@ -152,7 +147,7 @@ var myapp = angular.module('trashApp', [
             
           };
 
-        });
+        }]);
 
 
 
